62f7c30325edce817ad3bd3bfe0bd2cd94eb541c
Mingde (Matthew) Zeng <matthewzmd@posteo.net>
2025-04-28 19:27:17 -0400

    fix(parse): Replace regex with hand-rolled parser
    Backport 9894c1d
    
    Fixes CVE-2021-3803

--- parse.js
+++ parse.js
@@ -1,40 +1,84 @@
 module.exports = parse;
 
-//following http://www.w3.org/TR/css3-selectors/#nth-child-pseudo
+var ZERO = "0".charCodeAt(0);
+var NINE = "9".charCodeAt(0);
 
-//[ ['-'|'+']? INTEGER? {N} [ S* ['-'|'+'] S* INTEGER ]?
-var re_nthElement = /^([+\-]?\d*n)?\s*(?:([+\-]?)\s*(\d+))?$/;
+function isWhitespace(code) {
+    return (
+        code === 9  || // '\t'
+        code === 10 || // '\n'
+        code === 12 || // '\f'
+        code === 13 || // '\r'
+        code === 32    // ' '
+    );
+}
 
-/*
-	parses a nth-check formula, returns an array of two numbers
-*/
-function parse(formula){
-	formula = formula.trim().toLowerCase();
 
-	if(formula === "even"){
-		return [2, 0];
-	} else if(formula === "odd"){
-		return [2, 1];
-	} else {
-		var parsed = formula.match(re_nthElement);
+function parse(formula) {
+    formula = String(formula).trim().toLowerCase();
 
-		if(!parsed){
-			throw new SyntaxError("n-th rule couldn't be parsed ('" + formula + "')");
-		}
+    if (formula === "even") return [2, 0];
+    if (formula === "odd")  return [2, 1];
 
-		var a;
+    var idx = 0;
 
-		if(parsed[1]){
-			a = parseInt(parsed[1], 10);
-			if(isNaN(a)){
-				if(parsed[1].charAt(0) === "-") a = -1;
-				else a = 1;
-			}
-		} else a = 0;
+    var a = 0;
+    var sign = readSign();
+    var number = readNumber();
 
-		return [
-			a,
-			parsed[3] ? parseInt((parsed[2] || "") + parsed[3], 10) : 0
-		];
-	}
+    if (idx < formula.length && formula.charAt(idx) === "n") {
+        idx++;
+        a = sign * (number !== null ? number : 1);
+
+        skipWhitespace();
+
+        if (idx < formula.length) {
+            sign = readSign();
+            skipWhitespace();
+            number = readNumber();
+        } else {
+            sign = 0;
+            number = 0;
+        }
+    }
+
+    if (number === null || idx < formula.length) {
+        throw new SyntaxError("n-th rule couldn't be parsed ('" + formula + "')");
+    }
+
+    return [a, sign * number];
+
+    function readSign() {
+        if (formula.charAt(idx) === "-") {
+            idx++;
+            return -1;
+        }
+        if (formula.charAt(idx) === "+") {
+            idx++;
+        }
+        return 1;
+    }
+
+    function readNumber() {
+        var start = idx;
+        var value = 0;
+
+        while (idx < formula.length) {
+            var code = formula.charCodeAt(idx);
+            if (code >= ZERO && code <= NINE) {
+                value = value * 10 + (code - ZERO);
+                idx++;
+            } else {
+                break;
+            }
+        }
+
+        return idx === start ? null : value;
+    }
+
+    function skipWhitespace() {
+        while (idx < formula.length && isWhitespace(formula.charCodeAt(idx))) {
+            idx++;
+        }
+    }
 }
